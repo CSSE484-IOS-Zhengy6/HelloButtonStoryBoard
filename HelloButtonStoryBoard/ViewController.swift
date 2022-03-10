8868c1c998378bbe85620902e32869409bdc3414
travisz <zhengy6@rose-hulman.edu>
2022-03-10 09:16:19 -0500

    finished

--- HelloButtonStoryBoard/ViewController.swift
+++ HelloButtonStoryBoard/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    var counter=1;
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateView()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func pressedButton(_ sender: Any) {
+        let button : UIButton=sender as! UIButton
+        print(button.tag)
+        if button.tag == -1{
+            counter-=1
+        }else if button.tag == 0 {
+            counter=0
+        }else if button.tag==1 {
+            counter+=1
+        }else{
+            print("Something went wrong! Unexpected tag")
+        }
+        
+        updateView()
+    }
+    
+    func updateView(){
+        label.text="Counter= \(counter)"
+    }
 }
 
